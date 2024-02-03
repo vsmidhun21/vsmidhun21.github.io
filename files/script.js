d808aa38d2a646f88f710e308a566abd170374be
MIDHUN V S <114806736+vsmidhun21@users.noreply.github.com>
2024-02-03 14:39:03 +0530

    Add files via upload

--- files/script.js
+++ files/script.js
@@ -17,3 +17,20 @@ window.addEventListener("scroll",() =>{
         toTop.classList.remove("active");
     }
 })
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const cards = document.querySelectorAll('.reveal');
+    function checkScroll() {
+    cards.forEach(card => {
+        const rect = card.getBoundingClientRect();
+        const windowHeight = window.innerHeight;
+        if (rect.top < windowHeight * 0.75) {
+            card.style.opacity = 1;
+            card.style.transform = 'translateY(0)';
+        }
+    });
+    }
+    checkScroll();
+    window.addEventListener('scroll', checkScroll);
+});
