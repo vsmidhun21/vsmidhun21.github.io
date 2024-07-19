35e7df39c92d63ade90fb1b75c6da47f4f606078
vsmidhun21 <midhun890390@gmail.com>
2024-07-19 13:17:47 +0530

    New Update

--- files/script.js
+++ files/script.js
@@ -1,6 +1,39 @@
+/*=========Navbar Scroll===========*/
+document.addEventListener("DOMContentLoaded", function() {
+    const navbar = document.getElementById("header");
+    window.addEventListener("scroll", function() {
+      if (window.scrollY > 50) { // Adjust the value to your preference
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+    });
+});
 
+
+/*========== scroll sections active link in navbar ==========*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
+
+
+/*========== Typing animation in home page ==========*/
 var typed = new Typed(".text", {
-    strings: ["Programming" , "Cybersecurity" , "Web Development"],
+    strings: ["Programming" , "Web Development", "Cybersecurity"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
@@ -8,6 +41,7 @@ var typed = new Typed(".text", {
 });
 
 
+/*========== Go top icon in left bottom ==========*/
 const toTop = document.querySelector(".top");
 window.addEventListener("scroll",() =>{
     if (window.pageYOffset > 100){
@@ -18,7 +52,7 @@ window.addEventListener("scroll",() =>{
     }
 })
 
-
+/*========== Scroll Reveal script ==========*/
 document.addEventListener('DOMContentLoaded', function () {
     const cards = document.querySelectorAll('.reveal');
     function checkScroll() {
